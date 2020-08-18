db4c60b886909d2c324c63f9f55a9c9c8eac0c24
Bianca Silmaro <bianca@thinkingmachin.es>
2020-08-19 00:38:42 +0800

    Add images in gallery

---
+++ static/captions.js
@@ -0,0 +1,48 @@
+const captions = [
+  "Photos by Ian Keefe and Mike Von on Unsplash.", // 1
+  "Photos by Kevin Chinchilla, David Todd McCarty, and Ritesh Mandaliya on Unsplash.", // 2
+  "Photos by Zhang Kenny and Marit Gijsberts on Unsplash.", // 3
+  "Photos by Piero Regnante and Edwards Lee on Unsplash.", // 4
+  "Photos by Janita Sumeiko, Yucel Moran, and Fredd Rivett from Unsplash.", // 5
+  "Photos by Efe Kurnaz, Pine Watt, Alex Block, and Hiep Duong on Unsplash.", // 6
+  "Photos by Diana Simumpande, Morning Brew, and LOGAN WEAVER on Unsplash.", // 7
+  "Photos by Collin Clyne, Biel Morro, and Erol Ahmed on Unsplash.", // 8
+  "Photos by Jorgen Hendriksen, Toa Heftiba, and Jon Tyson on Unsplash.", // 9
+  "Photos by rotekirsche20, Monika Grabkowska, Jeremy Thomas, Johny Goerend, and Apostolos Vamvouras on Unsplash.", // 10
+  "Photos by Anomaly, Mona Eendra, and Paula Borowska on Unsplash.", // 11
+  "Photos by Rayyu Maldives photographer and Gabriele Stravinskaite on Unsplash.", // 12
+  "Photos by Organic Spirit Denmark and Nick Shandra on Unsplash.", // 13
+  "Photos by Chris Mok, Paolo Nicolello, and SOCIAL CUT on Upslash.", // 14
+  "Photos by Taylor Simpson, Bilal O., and Andrew 'Donovan' Valdivia on Unsplash.", // 15
+  "Photos by Roland Hechanova, Pawel Czerwiński, and Claudia Venables on Unsplash.", // 16
+  "Photos by Joshua Coleman and Jimmy Fermin on Unsplash.", // 17
+  "Photos by Joshua Coleman and Bernard Hermant on Unsplash.", // 18
+  "Photos by Steve Johnson, Julian Myles, and Eberhard Grossgasteiger on Unsplash.", // 19
+  "Photos by Dan Gold, Library of Congress, and Matthias Heyde on Unsplash.", // 20
+  "Photos by Joshua Coleman, Giorgio Trovato, Markus Spiske, and Alessia Chinazzo on Unsplash.", // 21
+  "Photos by Geordanna Cordero, Reid Zura, Ruslan Bardash, Pedro Lastra, and Suan Moo on Unsplash.", // 22
+  "Photos by Toa Heftiba and Jon Tyson on Unsplash.", // 23
+  "Photos by Jean Philippe Delberghe, Free To Use Sounds, and Library of Congress on Unsplash.", // 24
+  "Photos by Library of Congress, Enzo Tommasi, and Rebecca on Unsplash.", // 25
+  "Photos by Zach Kadolph and Janine Robinson on Unsplash.", // 26
+  "Photos by Morning Brew, Cris Dinoto, and Mae Mu on Unsplash.", // 27
+  "Photos by Gabriela Vinca and Joseph Menjivar on Unsplash.", // 28
+  "Photos by AlexaGate, Aiony Haust, Ricardo Gomez Angel, and Dlanor S on Unsplash.", // 29
+  "Photos by Ryan McGuire on Gratisography and Long Truong and Marija Zaric on Unsplash.", // 30
+  "Photos by ZIPHAUS, Marten Bjork, BP Miller, and Bekky Bekks on Unsplash.",
+  "Photo by Tuyen Vo on Unsplash.", // 31
+  "Photos by Joeyy Lee, Meric Dagli, and Library of Congress on Unsplash.", // 32
+  "Photo by Jason Leung on Unsplash. Background from freepik.", // 33
+  "Photos by Cris Trung, George Pagan III, and Z S on Unsplash.", // 34
+  "", // shining yoongi
+  "", // hobie world
+  "", // namgi fields
+  "", // cassette hobi
+  "", // fe yoongi
+  "", // blueberry eyes
+  "", // 10000 hours
+  "", // respect namgi
+  "", // red swan
+]
+
+export default captions
